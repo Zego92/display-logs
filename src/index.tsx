92d091c4a0c909323bc5f39606469df3a757a399
Zego92 <Ax0245aa>
2024-04-27 17:46:18 +0200

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './app';
+import App from '@/app';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
